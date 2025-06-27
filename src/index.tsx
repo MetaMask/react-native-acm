f993e1a0912e00015271042a8d2b3fe675babbc4
ieow <cwlee0@gmail.com>
2025-06-27 22:36:54 +0800

    Feat/acm error code (#7)
    Adds error code when invoked on ios
    
    ---------
    
    Co-authored-by: Chaitanya Potti <chaitanya.potti@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,23 @@
 import { NativeModules, Platform } from 'react-native';
 
+enum ACM_ERROR_CODE {
+  NOT_SUPPORTED_ON_IOS = 1,
+}
+
+const ACM_ERROR_CODE_MAP = {
+  [ACM_ERROR_CODE.NOT_SUPPORTED_ON_IOS]:
+    'Google ACM Sign In is not supported on iOS',
+};
+
+export class ACM_ERROR extends Error {
+  code: ACM_ERROR_CODE;
+  constructor(code: ACM_ERROR_CODE, message?: string) {
+    super(message || ACM_ERROR_CODE_MAP[code]);
+    this.name = 'ACM_ERROR';
+    this.code = code;
+  }
+}
+
 const LINKING_ERROR =
   `The package '@metamask/react-native-acm' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
@@ -38,9 +56,15 @@ export type GoogleCredential = {
 export function signInWithGoogle(
   params: GoogleSignInParams
 ): Promise<GoogleCredential> {
+  if (Platform.OS === 'ios') {
+    throw new ACM_ERROR(ACM_ERROR_CODE.NOT_SUPPORTED_ON_IOS);
+  }
   return GoogleAcm.signInWithGoogle(params);
 }
 
 export function signOut(): Promise<void> {
+  if (Platform.OS === 'ios') {
+    throw new ACM_ERROR(ACM_ERROR_CODE.NOT_SUPPORTED_ON_IOS);
+  }
   return GoogleAcm.signOut();
 }
